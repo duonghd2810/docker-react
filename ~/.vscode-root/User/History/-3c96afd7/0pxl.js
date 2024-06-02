2582479c7d91b0dc66d94d651a44f2b7992c2df8
root <root@DESKTOP-Q8141KC>
2024-06-02 22:54:20 +0700

    initital commit

---
+++ ~/.vscode-root/User/History/-3c96afd7/0pxl.js
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<App/>, div);
+});
+
+test('renders learn react link', () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<App/>, div);
+});
---
+++ ~/.vscode-root/User/History/-3c96afd7/Gk9a.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+test('renders learn react link', () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<App/>, div);
+});
